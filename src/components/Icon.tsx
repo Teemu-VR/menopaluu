c3611759891ace97799163d026b88d60add1472c
Vesa Piittinen <vesa.piittinen@vr.fi>
2022-02-18 14:20:25 +0200

    Fix crash after update

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { Icon, IconProps } from '@vr-core/web-components'
 import { default as VRLogo } from '@vr-core/web-components/icons/VRLogo.svg'
-import { useIsHydrated } from './Hydrate'
 
-export const VRLogoIcon = (props: IconProps) =>
-	// this check exists as a workaround due to <Icon /> not rendering on Astro SSR :(
-	useIsHydrated() ? (
-		<Icon src={VRLogo} {...props} />
-	) : (
-		<svg width={(props.size ?? 1.5) * 16} height={(props.size ?? 1.5) * 16} />
-	)
+import styles from './Icon.module.css'
+
+export const VRLogoIcon = ({ size = 1.5 }) =>
+	<svg  width={size * 16} height={size * 16} className={styles.icon}>
+		<use href={`${VRLogo}#icon`} />
+	</svg>
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import HydrateProvider from './Hydrate'
 
 import { VRLogoIcon } from './Icon'
 
 export function Test() {
 	return (
-		<HydrateProvider>
-			<VRLogoIcon size={5} />
-		</HydrateProvider>
+		<VRLogoIcon size={5} />
 	)
 }
